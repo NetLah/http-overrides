591c0b6dd0000f7037c19e6fcb661987ad8b3117
Tho Ho <code@tho.email>
2023-03-16 02:17:00 +0800

    Support .NET 8.0 (#2)
    * GitHub actions is default include in the solution
    
    * Consolidate Directory.Build.targets
    
    * Add .NET 8.0
    
    * Try upgrade C# 11
    
    * Fix pack nuget with --output
    
    * Push package to cloudsmith

--- samples/SampleWebApi/Program.cs
+++ samples/SampleWebApi/Program.cs
@@ -15,6 +15,10 @@ try
     builder.UseSerilog(logger => LogAppEvent(logger, "Application initializing...", appInfo));
     var logger = AppLog.Logger;
 
+    IAssemblyInfo assembly = new AssemblyInfo(typeof(HttpOverridesExtensions).Assembly);
+    logger.LogInformation("Library:{title}; Version:{version} BuildTime:{buildTime}; Framework:{framework}",
+        assembly.Title, assembly.InformationalVersion, assembly.BuildTimestampLocal, assembly.FrameworkName);
+
     // Add services to the container.
 
     builder.Services.AddControllers();
